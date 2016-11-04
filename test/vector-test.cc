e35a13a7dccf653f40b77098c01832e6c028a9d3
Li Wang <wangli1426@gmail.com>
2016-11-04 15:29:18 +0800

    supported nested definition of s_vector

--- test/vector-test.cc
+++ test/vector-test.cc
@@ -21,3 +21,14 @@ TEST(STATIC_VECTOR, RANDOM_ACCESS) {
     EXPECT_EQ(v[i], i);
   }
 }
+
+TEST(STATIC_VECTOR, RECURSIVE_DEFINE) {
+  s_vector<s_vector<int, 16>, 20> s;
+  for(int i = 0; i < 10; i++ ) {
+    s_vector<int, 16> tmp_v;
+    tmp_v.push_back(i);
+    s.push_back(tmp_v);
+  }
+  EXPECT_EQ(s.size(), 10);
+}  
+
