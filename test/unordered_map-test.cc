f781f2400362e60561172683a648a7daabfeb4a5
Li Wang <wangli1426@gmail.com>
2016-11-23 23:11:06 +0800

    added test cases for static_unordered_map

---
+++ test/unordered_map-test.cc
@@ -0,0 +1,31 @@
+#include "gtest/gtest.h"
+#include "lib/static_unordered_map.h"
+
+TEST(STATIC_UNORDERED_MAP, CONTAINS) {
+  static_unordered_map<int, int, 10> m;
+  m.insert(100,1);
+  EXPECT_EQ(m.contains(100), true);
+}
+
+TEST(STATIC_UNORDERED_MAP, ACCESS) {
+  static_unordered_map<int, int, 10> m;
+  m.insert(100, 1);
+  EXPECT_EQ(m[100], 1);
+}
+
+TEST(STATIC_UNORDERED_MAP, MULTIPLEINSERTION) {
+  static_unordered_map<int, int, 10> m;
+  m.insert(100, 100);
+  m.insert(101, 101);
+  m.insert(102, 102);
+  EXPECT_EQ(m[100], 100);
+  EXPECT_EQ(m[101], 101);
+  EXPECT_EQ(m[102], 102);
+}
+
+TEST(STATIC_UNORDERED_MAP, DUPLICATEDKEYS) {
+ static_unordered_map<int, int, 1> m;
+ m.insert(1, 100);
+ m.insert(1, 102);
+ EXPECT_EQ(m[1], 102);
+} 
